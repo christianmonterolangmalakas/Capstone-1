10b4076cf0d437e6ca1b188b9c183c1403c80958
christian montero <christianopogito123@gmail.com>
2024-11-05 16:24:54 +0800

    install some important files

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["lazada-recruitment.s3.us-east-1.amazonaws.com"],
+  },
+};
+
+export default nextConfig;
