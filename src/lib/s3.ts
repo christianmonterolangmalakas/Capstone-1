4f16bba58a8a1badc4c99d1e48bd80f7c734a5a5
christian montero <christianopogito123@gmail.com>
2024-11-10 17:07:23 +0800

    Update s3.ts

---
+++ src/lib/s3.ts
@@ -0,0 +1,15 @@
+import AWS from "aws-sdk";
+
+export async function uploadToS3(file: File, progressCallback?: (progress: number) => void) {
+  try {
+    AWS.config.update({
+      accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
+    });
+
+    const s3 = new AWS.S3({
+      params: {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+      },
+      region: "us-east-1",
+    });
