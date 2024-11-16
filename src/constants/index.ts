370563e59fb5f6e1ae5edc8033d3f7c536609d6c
christian montero <christianopogito123@gmail.com>
2024-11-16 21:02:56 +0800

    Update index.ts

---
+++ src/constants/index.ts
@@ -0,0 +1,21 @@
+export const MAX_UPLOAD_FILE_SIZE = 5242880; // 5MB
+export const DEFAULT_MAX_FILES = 5;
+export const DEFAULT_MIN_FILE = 1;
+export const OPT_LENGTH = 6;
+
+export enum FormFieldType {
+  INPUT = "input", // added
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput", // added
+  OTP_INPUT = "otpInput", // added
+  SELECT = "select", // added
+  RADIO = "radio", // added
+  CHECKBOX = "checkbox", //added
+  SWITCH = "switch",
+  SLIDER = "slider",
+  DATE_PICKER = "datePicker", // added
+  DROP_ZONE = "dropZone", // added
+  SKELETON = "skeleton",
+  HIDDEN = "hidden",
+  HONEY_POT = "honeyPot",
+}
