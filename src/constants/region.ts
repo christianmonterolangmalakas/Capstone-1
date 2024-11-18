d5ab30253e84be05e3340c4f53991833f2d4e407
christian montero <christianopogito123@gmail.com>
2024-11-18 22:51:22 +0800

    added code in region in personal job application

---
+++ src/constants/region.ts
@@ -0,0 +1,11 @@
+//create a function that will return all the regions in array
+export const getRegions = () => {
+  return region;
+};
+
+interface Region {
+  region_id: number;
+  region_name: string;
+  region_description: string;
+  FIELD4: string;
+}
