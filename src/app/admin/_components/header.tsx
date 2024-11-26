b0d3451e7edfe4c2ca4f855ddc18810931d6c8a3
christian montero <christianopogito123@gmail.com>
2024-11-26 18:55:41 +0800

    Create header.tsx

---
+++ src/app/admin/_components/header.tsx
@@ -0,0 +1,12 @@
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
