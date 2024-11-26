d11ae0b954f58d32c9a46e13b4037439f928d129
christian montero <christianopogito123@gmail.com>
2024-11-26 18:48:44 +0800

    Update app-sidebar.tsx

--- src/app/admin/_components/app-sidebar.tsx
+++ src/app/admin/_components/app-sidebar.tsx
@@ -14,3 +14,14 @@ import {
   Video,
 } from "lucide-react";
 
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { TeamSwitcher } from "./team-switcher";
+import { NavMain } from "./nav-main";
+import { NavProjects } from "./nav-projects";
+import { NavUser } from "./nav-user";
