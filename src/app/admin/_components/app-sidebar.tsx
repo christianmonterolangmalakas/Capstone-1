d94d20f2b9063ff4a6d13455a4ce175f9e016e22
christian montero <christianopogito123@gmail.com>
2024-11-26 18:48:06 +0800

    Create app-sidebar.tsx

---
+++ src/app/admin/_components/app-sidebar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import * as React from "react";
+import {
+  BrainCog,
+  CalendarCheck,
+  ChartArea,
+  ChartPie,
+  ClipboardPlus,
+  Megaphone,
+  SquareTerminal,
+  TableOfContents,
+  UserCog,
+  Video,
+} from "lucide-react";
+
