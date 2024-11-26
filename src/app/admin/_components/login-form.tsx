1fa6fc9bb0107ae57039239ca15a9808280e7ca2
christian montero <christianopogito123@gmail.com>
2024-11-26 18:57:25 +0800

    Create login-form.tsx

---
+++ src/app/admin/_components/login-form.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useState } from "react";
+import { toast } from "sonner";
+import { loginAdmin } from "@/actions/admin";
+import { useRouter } from "next/navigation";
+
+export const formSchema = z.object({
+  username: z.string().min(2, { message: "Username is required." }),
+  password: z.string().min(8, { message: "Password is required." }),
+});
+
