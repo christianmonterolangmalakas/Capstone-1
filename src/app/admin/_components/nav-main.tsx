658bb494da280903071a410c1e0e23666344a9af
christian montero <christianopogito123@gmail.com>
2024-11-28 22:00:12 +0800

    Update nav-main.tsx

--- src/app/admin/_components/nav-main.tsx
+++ src/app/admin/_components/nav-main.tsx
@@ -10,3 +10,18 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon?: LucideIcon;
+    isActive?: boolean;
+    items?: {
+      title: string;
+      url: string;
+    }[];
+  }[];
+}) {
+  return (
