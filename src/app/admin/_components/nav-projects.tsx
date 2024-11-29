001cb94c05cd93d764961317ceb5a14da00b5feb
christian montero <christianopogito123@gmail.com>
2024-11-29 23:44:42 +0800

    Update nav-projects.tsx

--- src/app/admin/_components/nav-projects.tsx
+++ src/app/admin/_components/nav-projects.tsx
@@ -23,4 +23,27 @@ export function NavProjects({
       url: string;
     }[];
   }[];
-}) {
+    }) {
+      return (
+        <SidebarGroup>
+          <SidebarGroupLabel>Others</SidebarGroupLabel>
+          <SidebarMenu>
+            {items.map((item) => (
+              <Collapsible
+                key={item.title}
+                asChild
+                defaultOpen={item.isActive}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <SidebarMenuButton tooltip={item.title}>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </Collapsible>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      );
+    }
