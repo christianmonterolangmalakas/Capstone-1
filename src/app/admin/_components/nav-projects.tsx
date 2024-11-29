77faee5a17b85a91f39bce23a6a12dfdca918f7a
christian montero <christianopogito123@gmail.com>
2024-11-29 23:42:36 +0800

    Create nav-projects.tsx

---
+++ src/app/admin/_components/nav-projects.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { type LucideIcon } from "lucide-react";
+
+import { Collapsible } from "@/components/ui/collapsible";
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
