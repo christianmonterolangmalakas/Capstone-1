415d6798fd948473e1b5be6065e750f28ccb726d
christian montero <christianopogito123@gmail.com>
2024-12-01 23:09:57 +0800

    Update team-switcher.tsx

--- src/app/admin/_components/team-switcher.tsx
+++ src/app/admin/_components/team-switcher.tsx
@@ -7,3 +7,12 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import Image from "next/image";
+
+export function TeamSwitcher() {
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <SidebarMenuButton size="lg">
+          <Image src="/logo.png" alt="Logo" width={40} height={40} />
+          <div className="grid flex-1 text-left text-sm leading-tight">
