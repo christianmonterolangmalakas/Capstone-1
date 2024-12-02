ea29f28926469f34285c2cb3e194a71bc630e4dd
christian montero <christianopogito123@gmail.com>
2024-12-02 22:10:31 +0800

    Update team-switcher.tsx

--- src/app/admin/_components/team-switcher.tsx
+++ src/app/admin/_components/team-switcher.tsx
@@ -8,20 +8,3 @@ import {
 } from "@/components/ui/sidebar";
 import Image from "next/image";
 
-export function TeamSwitcher() {
-  return (
-    <SidebarMenu>
-      <SidebarMenuItem>
-        <SidebarMenuButton size="lg">
-          <Image src="/logo.png" alt="Logo" width={40} height={40} />
-          <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-semibold">Lazada Logistics</span>
-            <span className="truncate text-xs">
-              Applican Information System
-            </span>
-          </div>
-        </SidebarMenuButton>
-      </SidebarMenuItem>
-    </SidebarMenu>
-  );
-}
